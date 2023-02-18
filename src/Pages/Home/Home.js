0975af708b25d4ba37dd78917bc18b66317ce9fa
Artur Danielyan <danieyanartur@gmail.com>
2023-02-18 20:04:05 +0100

    formatting code

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,35 +1,36 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from "react";
 import "./Styles/HomeStyles.scss";
-import Opening from './Sections/Opening';
-import AboutMe from './Sections/AboutMe';
-import Offer from './Sections/Offer';
-import MyStyle from './Sections/MyStyle';
-import MyWorkPattern from './Sections/MyWorkPattern';
-import ContactForm from './Sections/ContactForm';
+import Opening from "./Sections/Opening";
+import AboutMe from "./Sections/AboutMe";
+import Offer from "./Sections/Offer";
+import MyStyle from "./Sections/MyStyle";
+import MyWorkPattern from "./Sections/MyWorkPattern";
+import ContactForm from "./Sections/ContactForm";
 //router
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 //helmet
-import HelmetComponent from '../../SEO/HelmetComponent';
-import ogpic from '../../SEO/og-picture.png';
-
-
+import HelmetComponent from "../../SEO/HelmetComponent";
+import ogpic from "../../SEO/og-picture.png";
 
 const Home = () => {
-
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   const contactRef = useRef();
-  useEffect(()=>{
-    if(pathname === "/kontakt"){
-      window.scroll({ top: (contactRef.current.offsetTop), left: 0, behavior: 'smooth' })
+  useEffect(() => {
+    if (pathname === "/kontakt") {
+      window.scroll({
+        top: contactRef.current.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
     }
-  },[pathname])
+  }, [pathname]);
   return (
-    <div className='home-container'>
-      <HelmetComponent 
-            title='Happy Ever After - Organizacja ślubów i wesel w Poznaniu'
-            description='Organizacja ślubów na terenie Poznania i okolic. Skorzystaj z usługi kompleksowej lub częściowej organizacji ślubu z pomocą wedding plannera.'
-            type='webstie'
-            img={ogpic}
+    <div className="home-container">
+      <HelmetComponent
+        title="Happy Ever After - Organizacja ślubów i wesel w Poznaniu"
+        description="Organizacja ślubów na terenie Poznania i okolic. Skorzystaj z usługi kompleksowej lub częściowej organizacji ślubu z pomocą wedding plannera."
+        type="webstie"
+        img={ogpic}
       />
       <Opening></Opening>
       <AboutMe></AboutMe>
@@ -38,7 +39,7 @@ const Home = () => {
       <MyWorkPattern></MyWorkPattern>
       <ContactForm ref={contactRef}></ContactForm>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
